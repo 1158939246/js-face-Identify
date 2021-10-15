a7c941fa5388f8a1d8daedf7292c9d1ab606e73e
chiyongdong <1158939246@qq.com>
2021-10-15 15:42:35 +0800

    Signed-off-by: chiyongdong <1158939246@qq.com>

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
-// const video = document.getElementById('video')
+const video = document.getElementById('video')
 
-// const starVideo = () => {
-//   navigator.getUserMedia(
-//     { video: {} },
-//     (stream) => (video.srcObject = stream),
-//     (err) => console.log(err)
-//   )
-// }
-console.log(faceapi)
-faceapi.nets.tinyFaceDetector.loadFromUri('./models')
-// faceapi.nets.tinyFaceDetector.loadFromUri('https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights')
-// Promise.all([
-//   faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
-//   // faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
-//   // faceapi.nets.faxeRecognitionNet.loadFromUri('./models'),
-//   // faceapi.nets.faceExpressionNet.loadFromUri('./models'),
-// ]).then(starVideo())
+const starVideo = () => {
+  navigator.getUserMedia(
+    { video: {} },
+    (stream) => (video.srcObject = stream),
+    (err) => console.log(err)
+  )
+}
 
-// video.addEventListener('play', () => {
-//   setInterval(async() => {
-//     const detections = await faceapi
-//       .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-//       .withFaceLandmarks()
-//       .withFaceExpressions()
-//       console.log(detections)
-//   }, 100)
-// })
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri(
+    'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights'
+  ),
+  faceapi.nets.faceLandmark68Net.loadFromUri(
+    'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights'
+  ),
+  faceapi.nets.faceRecognitionNet.loadFromUri(
+    'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights'
+  ),
+  faceapi.nets.faceExpressionNet.loadFromUri(
+    'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights'
+  ),
+]).then(starVideo())
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+
+  const displaySize = {
+    width: video.width,
+    height: video.height,
+  }
+  faceapi.matchDimensions(canvas, displaySize)
+
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions()
+    if (detections.length) {
+      const resizeDetections = faceapi.resizeResults(detections, displaySize)
+      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+      faceapi.draw.drawDetections(canvas, resizeDetections)
+      faceapi.draw.drawFaceLandmarks(canvas, resizeDetections)
+      faceapi.draw.drawFaceExpressions(canvas, resizeDetections)
+    }
+  }, 100)
+})
